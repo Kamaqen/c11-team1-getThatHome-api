e7af463f53aa0b039de26f94a5e7aea47696a9e8
Dante Vasquez <jdantevc@gmail.com>
2023-10-10 11:23:06 -0500

    ADDED: model properties sketch

---
+++ app/controllers/api/v1/properties_controller.rb
@@ -0,0 +1,99 @@
+class Api::V1::PropertiesController < ApplicationController
+  def index
+    properties = Property.all
+    render json: properties, status: 200
+  end
+
+  def show
+    property = Property.find_by(id: params[:id])
+    if property
+      render json: property, status: 200
+    else
+      render json: {
+        error: "Property Not Found"
+      }
+    end  
+  end
+
+  def create
+    property = Property.new(
+      rent_value: prop_params[:rent_value],
+      bedrooms: prop_params[:bedrooms],
+      bathrooms: prop_params[:bathrooms],
+      property_type: prop_params[:property_type],
+      contract_type: prop_params[:contract_type],
+      image: prop_params[:image],
+      description: prop_params[:description],
+      address: prop_params[:address],
+      pet_friendly: prop_params[:pet_friendly],
+      area: prop_params[:area],
+      property_price: prop_params[:property_price],
+      maintenance_price: prop_params[:maintenance_price],
+      user_id: prop_params[:user_id],
+      is_active: prop_params[:is_active],
+    )
+    if property.save
+      render json: property, status: 200
+    else 
+      render json: {
+        error: "Error Creating..."
+      }
+    end
+  end
+
+  def update
+    property = Property.find_by(id: params[:id])
+    if property
+      property.update(
+        rent_value: params[:rent_value],
+        bedrooms: params[:bedrooms],
+        bathrooms: params[:bathrooms],
+        property_type: params[:property_type],
+        contract_type: params[:contract_type],
+        image:  params[:image],
+        description: params[:description],
+        address: params[:address],
+        pet_friendly: params[:pet_friendly],
+        area: params[:area],
+        property_price:  params[:property_price],
+        maintenance_price: params[:maintenance_price],
+        user_id: params[:user_id],
+        is_active: params[:is_active]
+      )
+      render json: "Property Record Updated Successfully!"
+    else
+      render json: {
+        error: "Property Not Found"
+      }
+    end  
+  end
+
+  def destroy
+    property = Property.find_by(id: params[:id])
+    if property
+      property.destroy
+      render json: "Property Has Been Deleted"
+
+    end
+  end
+
+  private
+  def prop_params
+    params.require(:property).permit([
+      :rent_value,
+      :bedrooms,
+      :bathrooms,
+      :property_type,
+      :contract_type,
+      :image,
+      :description,
+      :address,
+      :pet_friendly,
+      :area,
+      :property_price,
+      :maintenance_price,
+      :user_id,
+      :is_active
+    ])
+  end
+end
---
+++ db/migrate/20231010152555_create_properties.rb
@@ -0,0 +1,21 @@
+class CreateProperties < ActiveRecord::Migration[7.1]
+  def change
+    create_table :properties do |t|
+      t.integer :rent_value
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.string :property_type
+      t.string :contract_type
+      t.text :description
+      t.string :address
+      t.boolean :pet_friendly
+      t.string :area
+      t.integer :property_price
+      t.integer :maintenance_price
+      t.integer :user_id
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end
