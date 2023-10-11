fc0d62f7d616005ca81a629365310de2da8377af
Dante Vasquez <jdantevc@gmail.com>
2023-10-10 19:16:27 -0500

    FIXED: routes

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::PropertiesController < ApplicationController
+class PropertiesController < ApplicationController
   def index
     properties = Property.all
     render json: properties, status: 200
