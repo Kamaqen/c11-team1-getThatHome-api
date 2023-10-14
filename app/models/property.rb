efd0583c1da93755bf240e95e21821e4adb5a50f
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-10-14 11:29:57 -0500

    ADD: delete users properties old table

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,7 +3,7 @@ class Property < ApplicationRecord
   has_many :user_properties
   has_many :users, through: :user_properties
   
-  validates  :property_type, :operation_type,:address, :bedrooms, :bathrooms, :area, :url, presence: true
+  validates  :property_type, :operation_type,:address, :bedrooms, :bathrooms, :area, :urls, presence: true
   enum operation_type: { sale: 0, rent: 1 }
   enum property_type: { house: 0, department: 1 }
 end
---
+++ db/migrate/20231014161659_remove_users_properties.rb
@@ -0,0 +1,16 @@
+class RemoveUsersProperties < ActiveRecord::Migration[7.1]
+  def up
+    drop_table :users_properties
+  end
+
+  def down
+    create_table :users_properties do |t|
+      t.belongs_to :user
+      t.belongs_to :property
+      t.boolean :is_contacted
+      t.boolean :is_saved
+    end
+
+    add_index :users_properties, [:user_id, :property_id], unique: true
+  end
+end
