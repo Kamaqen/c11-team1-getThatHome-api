2c56360c40224029145c204e16f3557bf71cc272
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-10-14 00:07:43 -0500

    UPDATE: url column to accept 1 string

--- db/migrate/20231010152555_create_properties.rb
+++ db/migrate/20231010152555_create_properties.rb
@@ -6,7 +6,7 @@ class CreateProperties < ActiveRecord::Migration[7.1]
       t.integer :bathrooms
       t.integer :property_type
       t.integer :operation_type
-      t.text :urls, array: true, default: []
+      t.string :url
       t.text :description
       t.string :address
       t.boolean :pet_friendly
